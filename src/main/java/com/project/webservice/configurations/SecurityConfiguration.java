106219feb15ea5c4027824956a2f5f2e11915401
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-19 23:46:17 +0530

    spring security 5 password encoder implementation

--- src/main/java/com/project/webservice/configurations/SecurityConfiguration.java
+++ src/main/java/com/project/webservice/configurations/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package com.project.webservice.configurations;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -13,12 +11,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("root").password("root").roles("USER");
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("root").password(encoder.encode("root")).roles("USER");
 	}
 
 }
