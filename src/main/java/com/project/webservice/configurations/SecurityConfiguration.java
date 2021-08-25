37e6139e74616da26e59765cfe3ebe706e90ea83
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-25 21:47:57 +0530

    csrf disabled

--- src/main/java/com/project/webservice/configurations/SecurityConfiguration.java
+++ src/main/java/com/project/webservice/configurations/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.project.webservice.configurations;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,6 +14,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		auth.inMemoryAuthentication().withUser("root").password(encoder.encode("root")).roles("USER");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
 	}
 
 }
