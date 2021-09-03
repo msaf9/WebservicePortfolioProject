5c01df3bff1fedccd6f2e4037148bbdd024f7385
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-09-04 00:59:20 +0530

    WIP: api documentation using swagger

--- src/main/java/com/project/webservice/endpoints/CityEndpointV1.java
+++ src/main/java/com/project/webservice/endpoints/CityEndpointV1.java
@@ -16,6 +16,11 @@ import com.project.webservice.request.CityRequest;
 import com.project.webservice.response.CityResponse;
 import com.project.webservice.services.CityService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@ApiOperation(value = "/city", tags = "City Controller")
 @RestController
 @RequestMapping(path = "/city")
 public class CityEndpointV1 {
@@ -23,31 +28,41 @@ public class CityEndpointV1 {
 	@Autowired
 	private CityService cityService;
 
+	@ApiOperation(value = "Create city", response = String.class)
 	@PostMapping(path = "/add")
 	public String addNewCity(@RequestBody CityRequest cityRequest) {
 		return cityService.addNewCity(cityRequest);
 	}
 
+	@ApiOperation(value = "Fetch all cities", response = City.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "SUCCESS", response = City.class),
+			@ApiResponse(code = 401, message = "UNAUTHORIZED", response = City.class),
+			@ApiResponse(code = 403, message = "FORBIDDEN", response = City.class),
+			@ApiResponse(code = 404, message = "NOT FOUND") })
 	@GetMapping(path = "/all")
 	public List<City> viewAllCity() {
 		return cityService.viewAllCity();
 	}
 
+	@ApiOperation(value = "Delete city by id", response = String.class)
 	@DeleteMapping(path = "delete")
 	public String deleteCity(int cityId) {
 		return cityService.deleteCity(cityId);
 	}
 
-	@DeleteMapping(path = "/deleteAll" )
+	@ApiOperation(value = "Delete all cities", response = String.class)
+	@DeleteMapping(path = "/deleteAll")
 	public String deleteAll() {
 		return cityService.deleteAll();
 	}
-	
+
+	@ApiOperation(value = "Update city by id", response = CityResponse.class)
 	@PutMapping(path = "/update")
 	public CityResponse editCity(@RequestBody City cityRequest) {
 		return cityService.editCity(cityRequest);
 	}
-	
+
+	@ApiOperation(value = "Fetch city by id", response = City.class)
 	@GetMapping(path = "/city/{id}")
 	public City viewCity(int id) {
 		return cityService.viewCity(id);
--- src/main/java/com/project/webservice/request/CityRequest.java
+++ src/main/java/com/project/webservice/request/CityRequest.java
@@ -1,16 +1,26 @@
 package com.project.webservice.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@ApiModel(description = "City Request model")
 @Setter
 @Getter
 @NoArgsConstructor
 public class CityRequest {
 	
+	@ApiModelProperty(notes = "Name of the City", name = "city name", required = true, value = "Boston")
 	private String cityName;
+	
+	@ApiModelProperty(notes = "Description of the City", name = "city Description", required = true, value = "Boston description")
 	private String cityDescription;
+	
+	@ApiModelProperty(notes = "State of the City located", name = "state", required = true, value = "MAS")
 	private String state;
+	
+	@ApiModelProperty(notes = "Country of the State located", name = "country", required = true, value = "USA")
 	private String country;
 }
